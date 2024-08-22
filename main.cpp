7b799ce96da17f8ea4fcb71c5cd8281607497aab
Văn Hiệp <112191296+vhiepp@users.noreply.github.com>
2024-08-22 23:08:47 +0700

    Initialize template-cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// HICOMMIT TEMPLATE FOR C++
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    /*
+        Your code goes here. Happy coding!
+    */
+
+    return 0;
+}
