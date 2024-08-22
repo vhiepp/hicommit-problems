f49b09f1d84f0b1ed85064e3a5b2b0a32920894c
Văn Hiệp <112191296+vhiepp@users.noreply.github.com>
2024-08-22 23:08:48 +0700

    Initialize template-java

---
+++ Main.java
@@ -0,0 +1,13 @@
+// HICOMMIT TEMPLATE FOR JAVA
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+            Your code goes here. Happy coding!
+        */
+
+    }
+}
