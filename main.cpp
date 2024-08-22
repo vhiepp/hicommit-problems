3241af5eebe59d4c94efac16d8ee779fb56b431e
Văn Hiệp <112191296+vhiepp@users.noreply.github.com>
2024-08-22 23:08:57 +0700

    Submit code at 8/23/2024, 12:08:42 AM by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// HICOMMIT TEMPLATE FOR C++
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -6,10 +5,6 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    /*
-        Your code goes here. Happy coding!
-    */
-
+    
     return 0;
 }
